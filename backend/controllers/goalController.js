2371237ac6ff51d8280fbd515307218945fbfb1a
m4rkbello <markbello10041999@gmail.com>
2024-10-27 10:17:40 +0000

    CONTROLLER

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,46 @@
+// @desc Get goals
+// @route Get /api/goals
+// @access Private
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals!'})
+}
+
+// @desc Set goals
+// @route POST /api/goal
+// @access Private
+const setGoal = (req, res) => {
+    console.log(req.body)
+
+    if(!req.body.text){
+        res.status(404)
+        throw new Error('Please add a text field!')
+    }
+    res.status(200).json({message: 'ADD OHAHA!'})
+}
+
+// @desc Update goals
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoal = (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+}
+
+
+// @desc delete goals
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+}
+
+
+
+
+
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+}
