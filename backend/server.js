f5b2c47599ca73b40552442cb601642dc2b5c37d
m4rkbello <markbello10041999@gmail.com>
2024-10-27 10:17:21 +0000

    FAMILIARIZE THE SYNTAX - SERVER

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("PATOTOYA!");
+const express = require('express');
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalROutes'))
+
+
+app.listen(port, () => console.log(`SERVER HAS BEEN STARTED ON PORT ${port}`))
