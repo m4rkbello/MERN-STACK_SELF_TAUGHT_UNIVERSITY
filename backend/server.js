0970ff3e3cfcfffb34111f88cbe027db041866a8
m4rkbello <markbello10041999@gmail.com>
2024-10-28 13:55:51 +0000

    ERROR HANDLER

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -9,6 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalROutes'))
+
+app.use(errorHandler)
 
 
 app.listen(port, () => console.log(`SERVER HAS BEEN STARTED ON PORT ${port}`))
