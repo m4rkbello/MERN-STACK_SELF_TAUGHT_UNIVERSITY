870594e6fa3738944e65dfea409bd4a5eafc1199
m4rkbello <markbello10041999@gmail.com>
2024-10-29 13:40:21 +0000

    ADD THIS LIVE CONNECTION AND EXPOSE TO THE PUBLIC

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+//IMPORTING THE MONGOOSE
+const mongoose = require('mongoose')
+
+
+//connection
+const connectDB = async() => {
+    try  {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`LIVE MONGODB CONNECTED: ${conn.connection.host}`.lime.underline);
+    }catch  (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
