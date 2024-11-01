80883ba2c2fc26439bf5de1cea8c6d98c6b127be
m4rkbello <markbello10041999@gmail.com>
2024-11-01 20:35:34 +0800

    ADD USER ROUTE

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router()
 
 //FOR USER-REGISTER
 const { registerUser, loginUser, getMe } = require('../controllers/userController')
+const {protected} = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protected, getMe)
 
 module.exports = router
