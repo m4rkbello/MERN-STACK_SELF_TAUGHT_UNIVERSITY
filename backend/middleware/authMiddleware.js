924bab7f85fd500cba181cfc2683ac5289db3ebb
m4rkbello <markbello10041999@gmail.com>
2024-11-01 20:32:38 +0800

    ADD FILE FOR AUTHENTICATION MIDDLEWARE

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,36 @@
 const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+const protected = asyncHandler(async(req, res, next) => {
+    let Token 
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try{
+            //Get token from header
+            Token = req.headers.authorization.split(' ')[1]
+
+            //Verify token
+            const decoded = jwt.verify(Token, process.env.JWT_SECRET)
+
+            //Get user from the Token
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+        }catch(error){
+            console.log(error)
+            res.status(401)
+            throw new Error('Not authorized!')
+        }
+    }
+
+    if(!Token){
+        res.status(401)
+        throw new Error('Not authorized!')
+    }
+})
+
+module.exports = {
+    protected
+}
