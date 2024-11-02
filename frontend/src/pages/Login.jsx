e711cd7a1a7e63d267f0285f6b944b4f166d2f8a
m4rkbello <markbello10041999@gmail.com>
2024-11-02 19:53:12 +0800

    ADD

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react'
-import { FaSignInAlt } from 'react-icons/fa'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+//REDUXISM
+import { login, reset } from '../features/auth/authSlice'
+//COMPONENTS
+import Spinner from '../components/Spinner'
+//ICONS
+import { FaUser, FaSignInAlt } from 'react-icons/fa'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -12,22 +19,54 @@ function Login() {
     //destructuring - tagi2
     const {email, password} = formData
 
+    const redirectTo = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} =  useSelector((state) => state.auth)
+
+    //bantay
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess || user)  {
+            redirectTo('/')
+        }
+
+        dispatch(reset())   
+
+    },[user, isError, isSuccess, message, redirectTo, dispatch])
+
     //input setter
     const handleChangeFormData = (e) => {
         setFormData((prevState) => ({
             ...prevState,
-            [e.target.email]: e.target.value,
+            [e.target.name]: e.target.value,
         }))
     }
 
     const handleLoginSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email,
+            password,
+        }
+
+        dispatch(login(userData))
     }
+
+    //spinner or loading 
+    if(isLoading){
+        return <Spinner />
+    }
+    
 
     return <>
         <section className='heading'>
             <h1>
-                <FaSignInAlt />Register
+                <FaSignInAlt />Login
             </h1>
             <p>Login</p>
         </section>
