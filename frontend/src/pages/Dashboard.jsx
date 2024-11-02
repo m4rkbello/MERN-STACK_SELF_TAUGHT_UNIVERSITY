3e8111a3a779039a340533420ff69da1b543f0eb
m4rkbello <markbello10041999@gmail.com>
2024-11-02 23:05:47 +0800

    ADD

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+//REDUXISM
+import { register, reset } from '../features/auth/authSlice'
+//COMPONENTS
+import Spinner from '../components/Spinner'
+import GoalForm from '../components/GoalForm'
+//ICONS
+import { FaUser } from 'react-icons/fa'
 
 function Dashboard() {
+
+  const redirectTo = useNavigate()
+
+  const {user} = useSelector((state) => state.auth)
+
+
+  useEffect(() => {
+    if(!user) {
+      redirectTo('/login')
+    }
+
+    console.log(user);
+  },[user, redirectTo])
+
   return (
     <>
-      <div>
-        DASHBOARD
-      </div>
+    <section className="heading">
+      <h1>Welcome {user ? user.name : ''}</h1>
+      <h4>{user ? user.email : ''}</h4>
+      <p>TODO-MERN Dashboard</p>
+    </section>
+    <GoalForm />
     </>
   )
 }
