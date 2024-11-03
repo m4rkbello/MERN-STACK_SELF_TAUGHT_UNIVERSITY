f6915bba88d665e943f7082258ac08ab27f299b4
m4rkbello <markbello10041999@gmail.com>
2024-11-03 17:09:37 +0800

    ALAW UTSBO

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -15,42 +15,6 @@ export const goalSlice = createSlice({
     reducers:   {
         reset: (state) => initialState
     },
-    //reducers
-    // extraReducers: (builder) => {
-    //     builder
-    //         .addCase(register.pending, (state) => {
-    //             state.isLoading = true
-    //         })
-    //         .addCase(register.fulfilled, (state, action) => {
-    //             state.isLoading = false
-    //             state.isSuccess = true
-    //             state.user = action.payload
-    //         })
-    //         .addCase(register.rejected, (state, action) => {
-    //             state.isLoading = false
-    //             state.isError = true
-    //             state.message = action.payload
-    //             state.user = null
-    //         })
-    //         .addCase(login.pending, (state) => {
-    //             state.isLoading = true
-    //         })
-    //         .addCase(login.fulfilled, (state, action) => {
-    //             state.isLoading = false
-    //             state.isSuccess = true
-    //             state.user = action.payload
-    //         })
-    //         .addCase(login.rejected, (state, action) => {
-    //             state.isLoading = false
-    //             state.isError = true
-    //             state.message = action.payload
-    //             state.user = null
-    //         })
-    //         .addCase(logout.fulfilled, (state) => {
-    //             state.user = null
-    //         })
-
-    // }
 })
 
 export const {reset} = goalSlice.actions
