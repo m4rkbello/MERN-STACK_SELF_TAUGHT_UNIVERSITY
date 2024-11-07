0cc25248b2a6a20d522f00fc5ebb462ca8de8e66
m4rkbello <markbello10041999@gmail.com>
2024-11-07 22:01:11 +0800

    tug ta tog

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,24 +1,37 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import {createGOal} from '../features/goals/goalSlice'
 
 function GoalForm() {
   const [text, setText] = useState('')
-  
 
-  const handleFormSubmit = e => {
+
+  //ADD useDispatch of react-redux to pass the data on it's action
+  const dispatch = useDispatch()
+
+//function para ipasa sa redux
+  const handleOnSubmitAddGoal = e => {
     e.preventDefault()
+    dispatch(createGoal({text}))
+    setText('')
   }
 
 
   return <section className='form'>
+    <div className='form-group'>
+
     <form onSubmit='handleFormSubmit'>
 
       <label htmlFor='text'>Goal</label>
-      <input type='text' />
+      <input type='text' name='text' id='text' value={text} onChange={(e) => setText()}  />
     </form>
+    </div>
+    <div className='form-group'>
+      <button className='btn btn-block' type='submit'>Add Goal</button>
+    </div>
 
   </section>
 
