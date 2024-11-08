d2fd1e5208b36a38a57c442380d34424eb795b68
m4rkbello <markbello10041999@gmail.com>
2024-11-08 22:24:37 +0800

    REDUX

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import goalService from './goalService'
 
 const initialState = {
     goals: [],
@@ -9,14 +11,46 @@ const initialState = {
     message: ''
 }
 
+//Create new goal
+export const createGoal = createAsyncThunk('goals/create', async (goalData, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token  // Ensure this token is correct
+        return await goalService.createGoal(goalData, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+
+
+
 export const goalSlice = createSlice({
     name: 'goal',
     initialState,
     reducers:   {
         reset: (state) => initialState
     },
+    
+    extraReducers: (builder) => {
+        builder
+            .addCase(createGoal.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(createGoal.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.goals.push(action.payload);
+            })
+            .addCase(createGoal.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            });
+    }
+    
 })
 
 export const {reset} = goalSlice.actions
 
-export const goalReducer = goalSlice.reducer; 
+export const goalReducer = goalSlice.reducer;
