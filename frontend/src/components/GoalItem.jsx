fb449bb221804300da09e05dd10580724d34a486
m4rkbello <markbello10041999@gmail.com>
2024-11-09 23:33:37 +0800

    ADD

---
+++ frontend/src/components/GoalItem.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteGoal } from '../features/goals/goalSlice';
+
+function GoalItem({ goal }) {
+  if (!goal) return null;  // Handle the case where goal might be undefined
+
+const dispatch = useDispatch()
+
+const deleteGoal = () => {
+
+}
+
+
+  return (
+    <div className='goal'>
+      <div>
+        {new Date(goal.createdAt).toLocaleString('en-US')}
+      </div>
+      <h2>{goal.text}</h2>
+      <button
+      onClick={() => dispatch(deleteGoal(goal._id))}
+       className="close"
+       >X</button>
+    </div>
+  )
+}
+
+export default GoalItem
