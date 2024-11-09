ab70302174d4e68c5460b90fb0ec90fb05fad696
m4rkbello <markbello10041999@gmail.com>
2024-11-09 22:22:12 +0800

    FRONTEND: ADD CUSTOMIZE SPINNER

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Spinner() {
   return (
-    <div className='loadingSpinnerContainer '>
-      <div className="loadingSPinner">
+    <div className='loadingSpinnerContainer'>
+      <div className="loadingSpinner">
         
       </div>
     </div>
